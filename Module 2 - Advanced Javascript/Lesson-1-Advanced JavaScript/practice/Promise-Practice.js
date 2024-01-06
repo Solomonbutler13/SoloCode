4c5791e1ca6f756c5da3098e18eb3065b2899dee
Solomon <solomonbutler13sb@gmail.com>
2024-01-05 22:04:20 -0500

    new

--- Module 2 - Advanced Javascript/Lesson-1-Advanced JavaScript/practice/Promise-Practice.js
+++ Module 2 - Advanced Javascript/Lesson-1-Advanced JavaScript/practice/Promise-Practice.js
@@ -21,13 +21,34 @@ new Promise( (res) => {res('h')}).then(res => console.log(res, 'i'))
 // 2.   Create a new promise for something you may or may not do. 
 //      After a few seconds log whether it has been done.
 
+const walkTheDog = new Promise((res, rej) => {
+    let weather = 'nice'
+    setTimeout(() => {
+      if (weather === 'nice') {
+        res('Walk the dog')
+      } else {
+        rej('Not today')
+      }}, 3000)
+    })
+   
+    walkTheDog
+    .then((val) => console.log(val))
+    .catch((err) => console.log(err))
+   
+  setTimeout(() => {
+    walkTheDog
+      .then((val) => console.log(val))
+      .catch((err) => console.log(err))
+  }, 5000)
 
 // 3.   Using the prior example use .then and .catch to handle responses and rejections
-
+walkTheDog
+.then((val) => console.log(val))
+.catch((err) => console.log(err))
 
 
 // 4.   What does a promise resolve to?
-
+//A promise can resolve to one of two possible states: fulfilled (resolved) or rejected.
 
 
 // 5. What is the output of the following code?
@@ -50,3 +71,9 @@ promise.then(v => {
     console.log(v);
   });
 
+//
+2
+4
+8
+16
+//
